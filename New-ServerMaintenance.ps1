4484f9b063851146adda0e398618e6071a5c6241
Jesse <russelljt@gmail.com>
2023-05-23 14:04:35 -0400

    update line 80
    correcting for improper character creation when pulling via invoke-webrequest

--- New-ServerMaintenance.ps1
+++ New-ServerMaintenance.ps1
@@ -77,7 +77,7 @@ $addtlmaint = @() # Populate with additional maintenance types
     function Get-Roles {
         Invoke-WebRequest -Uri https://raw.githubusercontent.com/KSMC-TS/server-wellness-collector/main/roles.txt -OutFile C:\temp\roles.txt
         $includedroles = Get-Content -Path C:\temp\roles.txt
-        $roles = (($includedroles | ForEach-Object {[regex]::Escape($_)}) –join "|")
+        $roles = (($includedroles | ForEach-Object {[regex]::Escape($_)}) -join "|")
     
         $installedroles = Get-WindowsFeature | Where-Object {($_.InstallState -eq "Installed") -and ($_.name -match $roles)} | Select-Object Name
         
